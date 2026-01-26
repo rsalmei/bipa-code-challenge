1690d90d2009e6e88a5e5a4779fcdb34310786b9
Rogério Sampaio de Almeida <rsalmei@gmail.com>
2026-01-26 18:34:08 -0300

    dynamic ordering support, live with Lucas

--- src/routes.rs
+++ src/routes.rs
@@ -1,7 +1,9 @@
 use crate::connectivity::NodeConnectivity;
-use axum::{Json, extract::State, http::StatusCode};
+use crate::errors::AppError;
+use axum::Json;
+use axum::extract::{Query, State};
 use chrono::DateTime;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use surrealdb::Surreal;
 use surrealdb::engine::any::Any;
 
@@ -22,10 +24,17 @@ pub struct NodeConnectivityFilter {
     min_capacity: Option<f64>, // in BTC, as returned by the API.
 }
 
+#[derive(Debug, Deserialize)]
+pub struct NodeConnectivitySort {
+    order: Option<String>,
+}
+
 /// Handler for the GET /nodes endpoint.
+#[tracing::instrument(skip(db))] // do not log the database handle.
 pub async fn get_nodes_connectivity_handler(
     State(db): State<Surreal<Any>>,
     Query(filter): Query<NodeConnectivityFilter>,
+    Query(sort): Query<NodeConnectivitySort>,
 ) -> Result<Json<Vec<FriendlyNodeConnectivity>>, AppError> {
     if let Some(min_capacity) = filter.min_capacity
         && min_capacity < 0.0
@@ -35,17 +44,22 @@ pub async fn get_nodes_connectivity_handler(
         ));
     }
 
-    // prepare the SQL query based on the filter, while avoiding memory allocations.
-    let sql = match filter.min_capacity {
-        Some(_) => {
-            "SELECT * FROM (SELECT *, first_seen FROM ln_node_connectivity
-            WHERE capacity >= $min_capacity)"
-        }
-        None => "SELECT * FROM ln_node_connectivity",
+    // prepare the SQL query based on the filter, unfortunately allocating memory given the dynamic
+    // filter and sort parameters; it currently has six possible combinations already, too much for
+    // static strings alone (2 filter options * 3 sort options).
+    let mut sql = "SELECT * FROM ln_node_connectivity".to_owned();
+    if filter.min_capacity.is_some() {
+        sql.push_str("\nWHERE capacity >= $min_capacity")
     };
-    // fetch current nodes connectivity data from the database.
+    match sort.order.as_deref() {
+        Some("capacity") => sql.push_str("\nORDER BY capacity DESC"),
+        Some("first_seen") => sql.push_str("\nORDER BY first_seen ASC"),
+        _ => sql.push_str("\nORDER BY alias ASC"),
+    }
+
+    // fetch current nodes connectivity data from the database, using the SQL query prepared above.
     let mut response = db
-        .query(sql)
+        .query(&sql)
         .bind((
             "min_capacity", // no problem binding vars that are not used in the query.
             filter.min_capacity.map(|c| (c * 100_000_000.0) as u64),
